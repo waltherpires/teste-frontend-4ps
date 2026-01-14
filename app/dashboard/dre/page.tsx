9a4298053b06f285b8634ff8042f238c84193ae9
Walther Oliveira <waltherpires@hotmail.com>
2026-01-14 12:12:43 -0300

    chore: adiciona dockerfile

--- app/dashboard/dre/page.tsx
+++ app/dashboard/dre/page.tsx
@@ -9,7 +9,6 @@ export default function DREPage() {
   return (
     <div className="flex flex-col h-full">
       <Header title="DRE" description="Demonstrativo de Resultado do Exercício - Análise detalhada por período" />
-
       <div className="flex-1 p-6 space-y-6">
         <DREKPIs />
         <DRETable />
